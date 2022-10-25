79e1a308863cf16b9ce5fff11ee5c0b876514def
Konstantyn <k.buhantsev@gmail.com>
2022-10-25 12:28:07 +0300

    dffgfgh

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <>
+      <h2>404 Page not found</h2>
+    </>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { Home } from './Home';
+export { Movies } from './Movies';
+export { MovieDetails } from './MovieDetails';
+export { NotFound } from './NotFound';
