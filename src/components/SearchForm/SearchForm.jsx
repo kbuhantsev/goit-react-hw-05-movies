5c1769d33726cedb5550e8ffdaea14f34124bce9
Konstantyn <k.buhantsev@gmail.com>
2022-10-26 23:53:12 +0300

    search form

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useSearchParams } from 'react-router-dom';
+
+export default function SearchForm() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get('query');
+  // console.log(query);
+
+  const formik = useFormik({
+    initialValues: {
+      input: query,
+    },
+    onSubmit: ({ input }) => {
+      setSearchParams({ query: input });
+    },
+    validationSchema: Yup.object({
+      input: Yup.string()
+        .min(2, 'Must be 2 characters or more')
+        .required('Required'),
+    }),
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="input">Film name:</label>
+      <input
+        id="input"
+        name="input"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.input}
+        placeholder="Batman"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,14 @@
+import SearchForm from 'components/SearchForm';
+// import { useSearchParams } from 'react-router-dom';
+
 export default function Movies() {
+  // const [searchParams] = useSearchParams();
+
+  // const query = searchParams.get('query');
   return (
     <>
       <p>Movies</p>
+      <SearchForm />
     </>
   );
 }
