9479f3f95a560c47fcb8e8d8c1111eb4fafba939
Konstantyn <k.buhantsev@gmail.com>
2022-11-04 17:24:29 +0200

    Update ScrollButton.styled.js

--- src/components/ScrollButton/ScrollButton.styled.js
+++ src/components/ScrollButton/ScrollButton.styled.js
@@ -1,4 +1,16 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const float = keyframes`
+  0% {
+    transform: translatey(0px);
+  }
+  50% {
+    transform: translatey(-10px);
+  }
+  100% {
+    transform: translatey(0px);
+  }
+`;
 
 export const Button = styled.div`
   position: fixed;
@@ -10,4 +22,5 @@ export const Button = styled.div`
   z-index: 1;
   cursor: pointer;
   color: ${p => p.theme.colors.accent};
+  animation: ${float} 2s ease-in-out infinite;
 `;
