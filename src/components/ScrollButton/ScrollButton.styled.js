cc835bf1c7af5ad29ffce41c78a9d6c903beceab
Konstantyn <k.buhantsev@gmail.com>
2022-11-04 17:27:54 +0200

    Update ScrollButton.styled.js

--- src/components/ScrollButton/ScrollButton.styled.js
+++ src/components/ScrollButton/ScrollButton.styled.js
@@ -15,8 +15,8 @@ const float = keyframes`
 export const Button = styled.div`
   position: fixed;
   width: 100%;
-  left: 90%;
-  bottom: 60px;
+  left: 95%;
+  bottom: 50px;
   height: 20px;
   font-size: 3rem;
   z-index: 1;
